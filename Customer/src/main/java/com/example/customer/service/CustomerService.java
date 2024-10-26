7f4dc57e9cb55718870f519be9a31c1283745a2c
DoVanMinhTai1 <22130238@st.hcmuaf.edu.vn>
2024-10-26 20:37:33 +0700

    fix

--- Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -1,7 +1,49 @@
 package com.example.customer.service;
 
+import com.example.customer.config.KeyCloakPropsConfig;
+import com.example.customer.viewmodel.customer.CustomerPostVm;
+import com.example.customer.viewmodel.customer.CustomerVm;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+    private static final String ERROR =  "%s: Client %s don't have access right for this resource";
+    private static final int USER_PAGE =2;
+    private final Keycloak keycloak;
+    private final KeyCloakPropsConfig keyCloakPropsConfig;
+
+
+    public CustomerService(Keycloak keycloak, KeyCloakPropsConfig keyCloakPropsConfig) {
+        this.keycloak = keycloak;
+        this.keyCloakPropsConfig = keyCloakPropsConfig;
+    }
+
+    public static CredentialRepresentation credentialRepresentation(String password) {
+        CredentialRepresentation passwordCredential = new CredentialRepresentation();
+        passwordCredential.setTemporary(false);
+        passwordCredential.setType(CredentialRepresentation.PASSWORD);
+        passwordCredential.setValue(password);
+        return passwordCredential;
+    }
+    private boolean checkUsernameExists(RealmResource realmResource,String username) {
+        List<UserRepresentation> users = realmResource.users().search(username,true);
+        return !users.isEmpty();
+    }
+    private boolean checkEmailExists(RealmResource realmResource,String email) {
+        List<UserRepresentation> users = realmResource.users().search(null,null,null,email,0,1);
+        return !users.isEmpty();
+    }
+
+//    public CustomerVm create(CustomerPostVm customerPostVm) {
+//        RealmResource realmResource = keycloak.realm(keyCloakPropsConfig.getRealm());
+//        if(checkUsernameExists(realmResource, customerPostVm.userName())) {
+//            throw
+//        }
+//    }
 }
--- book/src/main/java/com/example/book/BookWebApplication.java
+++ book/src/main/java/com/example/book/BookWebApplication.java
@@ -1,4 +1,4 @@
-package com.example.BookWeb;
+package com.example.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
