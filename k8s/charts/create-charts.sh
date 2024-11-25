61b9fae9882f2aba9360572dfa880162673ec34e
DoVanMinhTai1 <22130238@st.hcmuaf.edu.vn>
2024-11-26 00:00:27 +0700

    create chart

--- k8s/charts/create-charts.sh
+++ k8s/charts/create-charts.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-for chart in "book" ; do
+for chart in {"book","customer"} ; do
     helm create "$chart"
 
     # Remove all file except _helpers.tpl
-#    find ./"$chart"/templates/* ! -name '_helpers.tpl' -exec rm -rf {} +
+    find ./"$chart"/templates/* ! -name '_helpers.tpl' -exec rm -rf {} +
 
     # Overwrite values.yaml file
     values_template=$(cat values.template.yaml)
     values_file=${values_template//\{\{service\}\}/$chart}
-#    echo "$values_file" > ./"$chart"/values.yaml
+    echo "$values_file" > ./"$chart"/values.yaml
 
     # Overwrite Chart.yaml file
     chart_template=$(cat Chart.template.yaml)
     chart_file=${chart_template//\{\{service\}\}/$chart}
-#    echo "$chart_file" > ./"$chart"/Chart.yaml
+    echo "$chart_file" > ./"$chart"/Chart.yaml
 done
